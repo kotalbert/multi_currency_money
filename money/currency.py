73d5572317db0c46e732276dd2cc363e43253fed
Pawel Daniluk <pawel@daniluk.waw.pl>
2021-06-17 22:19:08 +0200

    Add Money class

--- money/currency.py
+++ money/currency.py
@@ -3,27 +3,22 @@ from __future__ import annotations
 from typing import Any
 
 
-class Dollar:
+class Money:
     def __init__(self, amount: float):
         self._amount = amount
 
-    def times(self, times: float) -> Dollar:
+    def times(self, times: float) -> Money:
         return Dollar(self._amount * times)
 
     def __eq__(self, other: Any) -> bool:
-        if not isinstance(other, Dollar):
+        if not isinstance(other, Money):
             return False
         return self._amount == other._amount
 
 
-class Franc:
-    def __init__(self, amount: float):
-        self._amount = amount
+class Dollar(Money):
+    pass
 
-    def times(self, times: float) -> Franc:
-        return Franc(self._amount * times)
 
-    def __eq__(self, other: Any) -> bool:
-        if not isinstance(other, Franc):
-            return False
-        return self._amount == other._amount
+class Franc(Money):
+    pass
