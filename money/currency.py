8f15aab04d38f4ab168e635546c1ffca7fa715f3
Pawel Daniluk <pawel@daniluk.waw.pl>
2021-06-17 22:04:19 +0200

    Make amount private

--- money/currency.py
+++ money/currency.py
@@ -7,14 +7,10 @@ class Dollar:
     def __init__(self, amount: float):
         self._amount = amount
 
-    @property
-    def amount(self):
-        return self._amount
-
     def times(self, times: float) -> Dollar:
         return Dollar(self._amount * times)
 
     def __eq__(self, other: Any) -> bool:
         if not isinstance(other, Dollar):
             return False
-        return self.amount == other.amount
+        return self._amount == other._amount
